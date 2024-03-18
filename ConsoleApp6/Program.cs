cb1c8cb220578ff3c79ad617e5a091d58f1c502e
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-19 00:43:50 +0400

    new

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //string n = Console.ReadLine();
+            //Name(n);
+
+
+            //Metod();
+
+
+
+            //Task1.
+            //string input = "hello";
+            //string b = char.ToUpper(input[0]) + input.Substring(1);
+
+
+            //Mainn(b);
+
+
+            //Task2.
+            //Method();
+
+
+            //Task3.
+            //Method1();
+
+
+
+            //Task5.
+            //string text = "radar";
+            //if (palindrom(text))
+            //{
+            //    Console.WriteLine(text + " palindromdur.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(text + "  palindrom deyil!");
+            //}
+
+        }
+
+        //Task1
+        //static void Name(string n)
+        //{
+
+        //     n = "Hello";
+
+
+        //    n = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(n.ToLower());
+
+        //    Console.WriteLine(n);
+
+        //    for (int i = n.Length - 1; i >= 0; i--)
+        //    {
+        //        Console.Write("");
+        //        Console.Write(n[i]);
+
+        //    }
+
+        //}
+
+
+
+
+
+
+
+
+
+        //Task4
+
+
+        //static void Metod()
+        //{
+        //    string text = "banana";
+        //    //for (int i = 0; i < text.Length; i++)
+        //    //{
+        //    //    Console.Write($"  {text[i]}");
+        //    //}
+        //    Console.WriteLine( text.Replace("a",""));
+
+        //}
+
+
+
+
+        //Task1.
+        //static void Mainn(string nn)
+        //{
+        //    char[] chars = nn.ToCharArray();
+        //    Array.Reverse(chars);
+        //    Console.WriteLine(chars);
+
+
+
+        //}
+
+
+
+
+        //Task2.
+        //static void Method()
+        //{
+        //    string text = "a::b::c::d::e";
+        //    Console.WriteLine(text.Replace("::"," "));
+        //}
+
+
+        //Task3.
+        //static void Method1()
+        //{
+        //    string text = "a b,c.d/e";
+        //    foreach (char item in text)
+        //    {
+        //        if(char.IsLetter(item))
+        //        {
+        //            Console.Write($" { item} ");
+        //        }
+        //    }
+        //}
+
+
+
+
+
+
+        //Task5.
+
+        //static bool palindrom(string text)
+        //{
+        //    for (int i = 0; i < text.Length / 2; i++)
+        //    {
+        //        if (text[i] != text[text.Length - i - 1])
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return true;
+        //}
+
+    }
+}
